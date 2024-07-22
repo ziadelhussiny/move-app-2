314478356bf228547e75252cc468fe20025c26f4
ziadelhussiny <143873867+ziadelhussiny@users.noreply.github.com>
2024-07-22 04:13:03 +0300

    update1.2

--- assets/js/cardAnimations.js
+++ assets/js/cardAnimations.js
@@ -1,37 +1,26 @@
-export function cardHoverIn() {
-  $(this).find($('.overlay')).css({"opacity":"1","visibility":"visible"});
-  $(this).find($('.overlay .title'))
-      .removeClass('animate__fadeOutUp animate__fadeOutDown')
-      .addClass('animate__fadeInUp animate__delay-0s animate__animated');
-  $(this).find($('.overlay .desc'))
-      .removeClass('animate__fadeOutLeft animate__fadeOutRight')
-      .addClass('animate__fadeInLeft animate__delay-0s animate__animated');
-  $(this).find($('.overlay .date'))
-      .removeClass('animate__fadeOutLeft animate__fadeOutRight')
-      .addClass('animate__fadeInRight animate__delay-0s animate__animated');
-  $(this).find($('.overlay .rate'))
-      .removeClass('animate__fadeOutUp animate__fadeOutDown')
-      .addClass('animate__fadeInDown animate__delay-0s animate__animated');
-  $(this).find($('.cardImage img'))
-      .removeClass('animate__zoomOut animate__rotateOut')
-      .addClass('animate__zoomIn animate__rotateIn animate__infinite animate__animated');
-}
-
-export function cardHoverOut() {
-  $(this).find($('.overlay')).css({"opacity":"0","visibility":"hidden"});
-  $(this).find($('.overlay .title'))
-      .removeClass('animate__fadeInUp animate__fadeInDown')
-      .addClass('animate__fadeOutUp animate__delay-0s animate__animated');
-  $(this).find($('.overlay .desc'))
-      .removeClass('animate__fadeInLeft animate__fadeInRight')
-      .addClass('animate__fadeOutLeft animate__delay-0s animate__animated');
-  $(this).find($('.overlay .date'))
-      .removeClass('animate__fadeInRight animate__fadeInLeft')
-      .addClass('animate__fadeOutRight animate__delay-0s animate__animated');
-  $(this).find($('.overlay .rate'))
-      .removeClass('animate__fadeInDown animate__fadeInUp')
-      .addClass('animate__fadeOutDown animate__delay-0s animate__animated');
-  $(this).find($('.cardImage img'))
-      .removeClass('animate__zoomIn animate__rotateIn animate__infinite')
-      .addClass('animate__zoomOut animate__rotateOut animate__animated');
-}
+export function cardHoverIn()
+    {
+      $(this).find($('.overlay')).css({"opacity":"1","visibility":"visible"});
+      $(this).find($('.overlay .title')).removeClass('animate__slideOutLeft');
+      $(this).find($('.overlay .title')).addClass('animate__fadeInDown animate__delay-0s');
+      $(this).find($('.overlay .desc')).removeClass('animate__slideOutLeft');
+      $(this).find($('.overlay .desc')).addClass('animate__flipInX animate__delay-0s');
+      $(this).find($('.overlay .date')).removeClass('animate__slideOutLeft');
+      $(this).find($('.overlay .date')).addClass('animate__fadeInUp animate__delay-0s');
+      $(this).find($('.overlay .rate')).removeClass('animate__slideOutLeft');
+      $(this).find($('.overlay .rate')).addClass('animate__fadeInUp animate__delay-0s');
+      $(this).find($('.cardImage img')).addClass("animate");
+  }
+ export function cardHoverOut()
+    {
+      $(this).find($('.overlay')).css({"opacity":"0","visibility":"hidden"});
+      $(this).find($('.overlay .title')).removeClass('animate__fadeInDown animate__delay-0s');
+      $(this).find($('.overlay .title')).addClass('animate__slideOutLeft');
+      $(this).find($('.overlay .desc')).removeClass('animate__flipInX animate__delay-0s');
+      $(this).find($('.overlay .desc')).addClass('animate__slideOutLeft');
+      $(this).find($('.overlay .date')).removeClass('animate__fadeInUp animate__delay-0s');
+      $(this).find($('.overlay .date')).addClass('animate__slideOutLeft');
+      $(this).find($('.overlay .rate')).removeClass('animate__fadeInUp animate__delay-0s');
+      $(this).find($('.overlay .rate')).addClass('animate__slideOutLeft');
+      $('.cardImage img').removeClass("animate");
+}  
